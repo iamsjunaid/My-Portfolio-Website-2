13f834081f90687b10cbda29208f6d45a111de2a
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-11 17:45:07 +0300

    Add email input interactivity

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamMenu = document.querySelector('.ham-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-icon');
 const mobileLinks = document.querySelectorAll('.mobile-link');
+const email = document.querySelector('.email');
 
 hamMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
@@ -15,4 +16,10 @@ mobileLinks.forEach((link) => {
   link.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
   });
+});
+
+email.addEventListener('input', () => {
+  if (email.validity) {
+    email.setCustomValidity('');
+  }
 });
